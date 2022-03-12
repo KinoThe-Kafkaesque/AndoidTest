f43f75e74b8bfc9d9043dc07619c6cc9f242a8cb
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-03-12 14:20:33 +0100

    animations added

--- app/src/main/java/two/one/showcase/SplashActivity.java
+++ app/src/main/java/two/one/showcase/SplashActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.logo);
+        Animation aniRotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anim);
+        logo.startAnimation(aniRotate);
         t1.start();
     }
     Thread t1 = new Thread(){
